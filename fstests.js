2babbeea9f78cfc36a6fd7f79fb492fd9de151e2
Your Name <you@example.com>
2018-12-08 18:24:23 +0400

    Creating modules

---
+++ fstests.js
@@ -0,0 +1,12 @@
+
+var obj = 
+{
+    "first_name": "Poxos",
+    "last_name": "Poxosyan",
+    "age": 13,
+    "tumo_student": true
+}
+
+var textJSON = JSON.stringify(obj);
+
+module.exports = textJSON;
--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-var express = require("express");
-var app = express();
+// var Square = require("./module.js");
+// var mySquareObject = new Square(5);
 
-app.use(express.static("public"));
+// function main() {
+//    console.log(mySquareObject.getArea());
+// }
+// main();
 
-app.get("/", function(req, res){
-   res.redirect("index.html");
-});
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+
+
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+
+// var fs = require("fs");
+// var dummyText = "Apple yep";
+
+// function main() {
+//     fs.writeFileSync("dummytext.txt", dummyText);
+//     var text = fs.readFileSync("dummytext.txt").toString();
+//     console.log(dummyText == text);
+//     console.log(text);
+//     fs.writeFileSync("undummytext.txt",
+//         text.replace("Apple", "Microsoft")
+//     );
+//  }
+//  main();
+
+
+
+
+
+var fs = require("fs");
+var FStest = require("./fstests.js");
+var textJSON = FStest;
+function main() {
+    fs.appendFileSync("obj.json",textJSON);
+}
+
+main();
